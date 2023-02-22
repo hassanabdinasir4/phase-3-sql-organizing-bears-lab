1700587a1270c4b1f6f64e5b9fd93efa3d4912c1
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2023-02-22 20:56:13 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (2,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (3,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (4,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (5,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (6,null, 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (7,"red", 1, "F", "purple", "hot", 1);
+INSERT INTO bears VALUES (8,"red", 1, "F", "purple", "hot", 1);
